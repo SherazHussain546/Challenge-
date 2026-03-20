6c43b595039578fd42d673d393fdb64473751fa6
Sheraz Hussain <sherazhussainofficial2@gmail.com>
2026-03-20 14:23:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8390073805-13598",
+  "appId": "1:686242920942:web:d37cd0a28e972ea160f6ff",
+  "apiKey": "AIzaSyCD42x-sLYsjiArzNoFIu6EBCF_8Gcbsmw",
+  "authDomain": "studio-8390073805-13598.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "686242920942"
+};
